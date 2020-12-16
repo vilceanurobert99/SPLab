6380c556d578dfd562df825f6cdd052987eabad3
vilceanurobert99 <72494948+vilceanurobert99@users.noreply.github.com>
2020-12-16 10:10:45 +0200

    lab6+7

---
+++ Lab6+7/src/Main.java
@@ -0,0 +1,19 @@
+import model.*;
+import services.OpenCommand;
+import services.SaveCommand;
+import services.StatisticsCommand;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        OpenCommand openCommand = new OpenCommand("MyBook");
+        openCommand.execute();
+        Section section = new Section("Chapter 2");
+        section.addElement(new Paragraph("First parag of chapter2"));
+        section.addElement(new Paragraph("Second parag of chapter2"));
+        SaveCommand saveCommand = new SaveCommand(section);
+        saveCommand.execute();
+        StatisticsCommand statisticsCommand = new StatisticsCommand();
+        statisticsCommand.execute();
+    }
+}
---
+++ Lab6+7/src/utils/DocumentManager.java
@@ -0,0 +1,27 @@
+package utils;
+
+import model.Book;
+
+public class DocumentManager {
+
+    private static DocumentManager documentManager;
+    private Book book;
+
+    private DocumentManager(){
+
+    }
+
+    public static DocumentManager getDocumentManager() {
+        if(documentManager == null)
+            documentManager = new DocumentManager();
+        return documentManager;
+    }
+
+    public void setBook(Book newBook) {
+        this.book = newBook;
+    }
+
+    public Book getBook() {
+        return this.book;
+    }
+}
